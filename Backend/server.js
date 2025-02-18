d71be4fe873ed2ce81406864c098b91b80766977
Sunny Kumar <sunnykumar49617@gmail.com>
2025-02-19 00:46:30 +0530

    add frontend url

--- Backend/server.js
+++ Backend/server.js
@@ -19,7 +19,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: "recyclelabanon.netlify.app",
     methods: "GET, POST, PUT, DELETE",
     optionsSuccessStatus: 200, // Some legacy browsers (IE11, various versions of Android) choke on 204
     credentials: true,
